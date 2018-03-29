2f85b52d24a0d637243c81439d7da29774f0a43d
paumon64 <pamafremon@gmail.com>
2018-03-29 19:29:11 +0100

    adicionar comentarios
    comments exercicios.

---
+++ src/soccer/League.java
@@ -0,0 +1,83 @@
+package soccer;
+
+public class League {
+
+	public static void main(String[] args) {
+
+		
+		// instanciar os players
+		
+		Player player1 = new Player();
+		player1.playerName = "George Eliot";
+		
+		Player player2 = new Player();
+		player2.playerName = "Graham Greene";
+		
+		Player player3 = new Player();
+		player3.playerName = "Geoffrey Chaucer";
+
+		
+		// instanciação directa do array
+		
+		Player[] thePlayers = { player1, player2, player3 };
+
+		Team team1 = new Team();
+		team1.teamName = "The Greens";
+		team1.playerArray = thePlayers;
+		
+		// alteração directa da variavel player1
+		// player1.playerName = "Robert Service";
+		
+		Team team2 = new Team();
+		team2.teamName = "The Reds";
+		
+		// criação do objecto e instaciação separadas
+		
+		team2.playerArray = new Player[3];
+		
+		
+		team2.playerArray[0] = new Player();
+		team2.playerArray[0].playerName = "Robert Service";
+		team2.playerArray[1] = new Player();
+		team2.playerArray[1].playerName = "Robbie Burns";
+		team2.playerArray[2] = new Player();
+		team2.playerArray[2].playerName = "Rafael Sabatini";
+/**
+ *  2 print the player names
+ **/
+		for (Player thePlayer : team1.playerArray) {
+
+			System.out.println(thePlayer.playerName);
+		}
+		
+		System.out.println("");
+		for (Player thePlayer : team2.playerArray) {
+
+			System.out.println(thePlayer.playerName);
+		}
+
+		//  create a game
+		
+		Game currGame = new Game();
+		currGame.homeTeam = team1;
+		currGame.awayTeam = team2;
+		
+		// 6.2 create a goals
+		
+		Goal goal1 = new Goal();
+		goal1.thePlayer = currGame.homeTeam.playerArray[2];
+		goal1.theTeam = currGame.homeTeam;
+		goal1.theTime = 55;
+	
+		Goal[] theGoals = {goal1};
+		currGame.goals = theGoals;
+		
+		System.out.println("");
+		
+		System.out.println("Goal scored after " +
+				currGame.goals[0].theTime + " mins by " +
+				currGame.goals[0].thePlayer.playerName + " of " +
+				currGame.goals[0].theTeam.teamName);
+		
+	}
+}
